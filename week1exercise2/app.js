596932bc8c6219291fb9d81095a880772af4b3de
janardhanRedde <badarijanardhanreddy912@gmail.com>
2022-10-12 20:48:55 +0530

    code

---
+++ week1exercise2/app.js
@@ -0,0 +1,16 @@
+var my=document.getElementById("myvideo")
+
+function playvideo(){
+  my.play();
+}
+
+function stopvideo(){
+  my.stop();
+}        
+
+function mutevideo(){
+  my.muted=true;
+}    
+function unmutevideo(){
+  my.muted=false;
+}   
